302759aba55a25709a20266bedc15021cbda27a5
amaleno <alvaromalenoaferez@gmail.com>
2023-09-16 14:13:50 +0200

    GIT-6: Creating ILand Interface

---
+++ App/Core/Models/Coordinates/Intefaces/ICoordinatesBase.cs
@@ -0,0 +1,7 @@
+namespace martianRobots.Core.Models.Base;
+
+public interface ICoordinatesBase
+{
+    public int x { get; set; }
+    public int y { get; set; }
+}
---
+++ App/Core/Models/Land/Interfaces/ILand.cs
@@ -0,0 +1,7 @@
+namespace martianRobots.Core.Models.Land;
+
+public interface ILand
+{
+    public int x { get; set; }
+    public int y { get; set; }
+}
